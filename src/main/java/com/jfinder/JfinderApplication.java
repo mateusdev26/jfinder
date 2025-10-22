b09abe9ddc471c3834ea74f72343286aa10bd73e
Mateus Oliveira <mateusoliveiradeveloper1@gmail.com>
2025-10-22 12:43:13 -0400

    First version

---
+++ src/main/java/com/jfinder/JfinderApplication.java
@@ -0,0 +1,13 @@
+package com.jfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JfinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jfinder/JfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
